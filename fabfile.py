0c6648fca9210dd4a5b40b012626521e3962ba9e
Hodonou SOUNTON <sounton@gmail.com>
2016-08-15 12:21:14 +0100

    new: test: permorfing the fabfile: cometics

--- fabfile.py
+++ fabfile.py
@@ -18,3 +18,4 @@ def ci(act='new', aud='dev', msg='empty', tag='wip'):
     local('git add .')
     local('git commit -m "%s: %s: %s: %s"' % (act, aud, msg, tag))
     local('gitchangelog > CHANGELOGS.md && git commit --amend')
+    run('print "Hello!"')
